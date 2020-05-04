41a269f6d3ad987f89fcb321e314b526f5c1ac7d
shin52 <b1018088@fun.ac.jp>
2020-05-04 16:53:26 +0900

    firstCommit

---
+++ TodoRealm/TodoRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoRealm
+//
+//  Created by Itou Shingo on 2020/05/04.
+//  Copyright © 2020 Itou Shingo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
