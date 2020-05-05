e5ebb5201254b3ee384aee71ba24ba22ec2c8f60
shin52 <b1018088@fun.ac.jp>
2020-05-05 20:53:00 +0900

    Alomost complete

--- TodoRealm/TodoRealm/ViewController.swift
+++ TodoRealm/TodoRealm/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    
 
+    var todoItems:Results<Todo>!
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        table.dataSource = self
+        
+        let realm = try! Realm()
+        todoItems = realm.objects(Todo.self)
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        table.reloadData()
+    }
+    
+    func deleteTodo(at index: Int){
+        let realm = try! Realm()
+        try! realm.write{
+            realm.delete(todoItems[index])
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let object = todoItems[indexPath.row]
+        cell.textLabel?.text = object.title
+        
+        return cell
+    }
 }
 
